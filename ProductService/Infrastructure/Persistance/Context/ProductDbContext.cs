d0402b06df1de648582dc85bf8eb8794c98ff40d
beyzacayabatmaz <byzcayabatmaaz@gmail.com>
2026-03-25 15:51:38 +0300

    İlk commit

---
+++ ProductService/Infrastructure/Persistance/Context/ProductDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BackendCaseStudy.BackendCaseStudy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendCaseStudy.BackendCaseStudy.Persistence.Contexts
+{
+    
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+
+        
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
